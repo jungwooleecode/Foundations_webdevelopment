c49c48af4974fa8c4310c88d8fe8c857cf7a8252
jungwooleecode <jung.lee@code.berlin>
2022-04-07 21:52:40 +0200

    login page done&new pages added

---
+++ showpw.js
@@ -0,0 +1,8 @@
+function visibleFunction() {
+    var x = document.getElementById("pwinput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
