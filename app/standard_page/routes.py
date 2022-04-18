39921e9101da716cecd495db7db58693e8e764b9
jungwooleecode <jung.lee@code.berlin>
2022-04-18 18:43:47 +0200

    updated

--- app/standard_page/routes.py
+++ app/standard_page/routes.py
@@ -6,3 +6,7 @@ blueprint = Blueprint('standard_page', __name__)
 @blueprint.route('/')
 def index():
     return render_template('standard_page/index.html')
+
+@blueprint.route('/about')
+def about():
+    return render_template('standard_page/about.html')
