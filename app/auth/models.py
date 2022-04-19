0e1597849a550427e201c40c5d4b3e8cd06bc950
jungwooleecode <jung.lee@code.berlin>
2022-04-20 00:16:53 +0200

    defined model

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,4 +1,6 @@
-class User():
-    id = ''
-    email=''
-    password=''
+from app.extensions.database import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email=db.Column(db.String(40), unique=True)
+    password=db.Column(db.String(20), unique=True)
