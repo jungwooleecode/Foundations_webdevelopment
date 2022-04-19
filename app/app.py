6c3aa005a8e2655cb05c91e082382968dc9426b2
jungwooleecode <jung.lee@code.berlin>
2022-04-19 23:42:28 +0200

    beginning of db

--- app/app.py
+++ app/app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from . import standard_page, booking, auth
+from app.extensions.database import db
 
 def create_app():
     app= Flask(__name__)
 
+    register_extensions(app)
     register_blueprints(app)
+
     return app
 
 def register_blueprints(app: Flask):
     app.register_blueprint(standard_page.routes.blueprint)
     app.register_blueprint(booking.routes.blueprint)
     app.register_blueprint(auth.routes.blueprint)
+
+def register_extensions(app: Flask):
+    db.init_app(app)
