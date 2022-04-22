ac4480f41e7e2108065ca7953389836e247b8717
jungwooleecode <jung.lee@code.berlin>
2022-04-22 13:16:06 +0200

    classinfo page appears corresponding to schedule

--- app/booking/models.py
+++ app/booking/models.py
@@ -3,14 +3,14 @@ from app.extensions.database import db
 
 class Dance(db.Model):
     id=db.Column(db.Integer, primary_key=True)
-    first:db.Column(db.String(20))
-    second:db.Column(db.String(20))
-    third:db.Column(db.String(20))
+    first=db.Column(db.String(20))
+    second=db.Column(db.String(20))
+    third=db.Column(db.String(20))
     level_id = db.Column(db.Integer, db.ForeignKey('level.id'))
 
 class Level(db.Model):
     id=db.Column(db.Integer, primary_key=True)
-    first:db.Column(db.String(10))
-    second:db.Column(db.String(10))
-    third:db.Column(db.String(10))
+    first=db.Column(db.String(10))
+    second=db.Column(db.String(10))
+    third=db.Column(db.String(10))
     dance = db.relationship('Dance', backref='level', uselist=False, lazy=True)
--- app/booking/routes.py
+++ app/booking/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from app import booking
 from .models import Dance,Level
+import datetime
 
 blueprint = Blueprint('booking', __name__)
 
@@ -8,14 +9,38 @@ blueprint = Blueprint('booking', __name__)
 def book():
     return render_template('booking/book.htm')
 
-@blueprint.route('/bookingcomplete')
+
+
+
+@blueprint.route('/bookingcomplete/')
 def bookingcomplete():
     return render_template('booking/bookingComplete.html')
 
-@blueprint.route('/classinfo')
-def classinfo():
-    return render_template('booking/classInfo.htm')
+
+
+
+@blueprint.route('/classinfo/<int:number>')
+def classinfo(number):
+    id=number//10+1
+    dance=Dance.query.filter_by(id=id).first_or_404()
+
+    time=number%10
+
+    if time==1:
+        strftime='5:30 - 6:50 PM'
+    elif time==2:
+        strftime='7:30 - 8:50 PM'
+    elif time==3:
+        strftime='9:30 - 10:50 PM'
+
+    return render_template('booking/classInfo.htm', dance=dance, time=time, strftime=strftime)
 
 @blueprint.route('/schedule')
 def schedule():
-    return render_template('booking/schedule.htm')
+    x = datetime.datetime.now()
+    year= x.year
+    month= x.strftime("%b")
+    day= x.day
+
+    all_dances=Dance.query.all()
+    return render_template('booking/schedule.htm', dances=all_dances, year=year, month=month, day=day)
