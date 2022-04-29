151c50417c439aa28f056aafc1ea0f197c609d49
jungwooleecode <jung.lee@code.berlin>
2022-04-29 12:25:52 +0200

    commit

--- migrations/versions/7eb90783a772_first.py
+++ migrations/versions/7eb90783a772_first.py
@@ -1,8 +1,8 @@
 """first
 
-Revision ID: 820507290581
+Revision ID: 7eb90783a772
 Revises: 
-Create Date: 2022-04-29 12:20:25.515138
+Create Date: 2022-04-29 12:25:10.029348
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '820507290581'
+revision = '7eb90783a772'
 down_revision = None
 branch_labels = None
 depends_on = None
